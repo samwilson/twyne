380025fec8a3976391f1c677a43a605fdc11fe51
Sam Wilson <sam@samwilson.id.au>
2020-09-24 18:33:51 +0800

    Add better Markdown/CommonMark support
    Conform to CommonMark syntax at least to some extent.

--- src/Markdown.php
+++ src/Markdown.php
@@ -2,13 +2,25 @@
 
 namespace App;
 
+/**
+ * This class formats *some parts* of Markdown, or rather CommonMark. It doesn't support everything (notably not HTML),
+ * and also adds a few extra things that are good only for this application.
+ *
+ * @link https://commonmark.org/help/
+ */
 class Markdown
 {
 
+    /**
+     * Convert Twyne-flavoured Markdown to HTML.
+     *
+     * @param string $in The Markdown source.
+     * @return string The (safe) HTML.
+     */
     public function toHtml(string $in): string
     {
         // Platform-independent newlines.
-        $out = preg_replace("/(\r\n|\r)/", "\n", $in);
+        $out = preg_replace("/(\r\n|\r)/", "\n", trim($in));
         // Paragraphs.
         $out = "<p>$out</p>";
         $out = preg_replace("|\n+\s*\n+|", "</p>\n\n<p>", $out);
@@ -18,22 +30,21 @@ class Markdown
         $out = preg_replace('|<p>>\s*(.*)|', "<blockquote>\n$1", $out);
         $out = preg_replace('|\n>\s*(.*)</p>|', "\n$1\n</blockquote>", $out);
         $out = preg_replace('|\n>\s(.*)|', "\n$1", $out);
+        // Separator.
+        $out = preg_replace("|<p>[-*]{3}</p>|", "<hr />", $out);
+        // Lists.
+        $out = preg_replace("|<p>\d\.(.*)|", "<ol>\n#$1", $out); // begin ordered
+        $out = preg_replace("|\d\.(.*)</p>|", "#$1\n</ol>", $out); // end ordered
+        $out = preg_replace("|<p>\*(.*)|", "<ul>\n*$1", $out); // begin unordered
+        $out = preg_replace("|\n\*(.*)</p>|", "\n*$1\n</ul>", $out); // end unordered
+        $out = preg_replace("|\n[*#]\s*(.*)|", "\n<li>$1</li>", $out); // list items
+        // Headings.
+        $out = preg_replace("|<p>#{4}\s*(.*)</p>|", "\n<h4>$1</h4>\n", $out);
+        $out = preg_replace("|<p>#{1,3}\s*(.*)</p>|", "\n<h3>$1</h3>\n", $out);
         // Emphasis.
         $out = preg_replace("|\*(.*?)\*|", "<em>$1</em>", $out);
         // Monospacing.
         $out = preg_replace("|`(.*?)`|", "<code>$1</code>", $out);
-        // Em Dashes.
-        $out = preg_replace("/---/", "&thinsp;&mdash;&thinsp;", $out);
-        // Ellipses.
-        $out = preg_replace("/\.\.\./", "&thinsp;&hellip;&thinsp;", $out);
-        // Lists.
-        $out = preg_replace("|<p>#(.*)|", "<ol>\n#$1", $out); // begin ordered
-        $out = preg_replace("|#(.*)</p>|", "#$1\n</ol>", $out); // end ordered
-        $out = preg_replace("|<p>\*(.*)|", "<ul>\n*$1", $out); // begin unordered
-        $out = preg_replace("|\*(.*)</p>|", "*$1\n</ul>", $out); // end unordered
-        $out = preg_replace("|[*#]\s*(.*)|", "<li>$1</li>", $out); // list items
-        // Headings.
-        $out = preg_replace("|<p>===\s*(.*)\s*===</p>|", "\n<h3>$1</h3>\n", $out);
 
         return trim($out);
     }

bdde9251a3b9e03ed19544ca5bcb73256e5ad8f3
Sam Wilson <sam@samwilson.id.au>
2020-09-26 18:31:56 +0800

    Add ESLint and a <time> converter
    And some style tweaks.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,2 @@
 import '../css/app.less';
-
+import './timezone-converter';
---
+++ assets/js/timezone-converter.js
@@ -0,0 +1,18 @@
+(function () {
+    document.body.querySelectorAll('time').forEach(function (timeEl) {
+        // Show the UTC time as the tooltip.
+        timeEl.title = 'UTC time: ' + timeEl.innerText;
+        // Convert to local browser time for actual display.
+        const date = new Date(Date.parse(timeEl.dateTime));
+        const options = {
+            timeZoneName: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            weekday: 'long',
+            hour: 'numeric',
+            minute: 'numeric'
+        };
+        timeEl.innerText = date.toLocaleString([], options) + '.';
+    });
+}());

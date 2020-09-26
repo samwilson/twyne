7e6406580202c82d185fae6bf7c1c932f26fa19c
Sam Wilson <sam@samwilson.id.au>
2020-09-26 15:38:37 +0800

    Fix main branch name in deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -19,23 +19,23 @@ git fetch --quiet origin 2>&1
 HIGHEST_TAG=$(git tag --list "*.*.*" | sort --version-sort | tail --lines 1)
 CURRENT_TAG=$(git describe --tags --always)
 CURRENT_BRANCH=$(git symbolic-ref --short -q HEAD)
-DIFF_TO_MASTER=$(git diff origin/master)
+DIFF_TO_MAIN=$(git diff origin/main)
 
 ## Prod site: do nothing if we're already at the highest tag.
 if [[ $MODE == 'prod' && $CURRENT_TAG == $HIGHEST_TAG ]]; then
     exit 0
 fi
 
-## Dev site: do nothing if not on master.
-if [[ $MODE == "dev" && $CURRENT_BRANCH != "master" ]]; then
+## Dev site: do nothing if not on main.
+if [[ $MODE == "dev" && $CURRENT_BRANCH != "main" ]]; then
     ## Tell the maintainers, so they don't forget they're in
     ## the middle of testing something.
-    echo "Dev site not on master branch. Not deploying."
+    echo "Dev site not on main branch. Not deploying."
     exit 0
 fi
 
-## Dev site: do nothing if there's no difference to master.
-if [[ $MODE == "dev" && -z "$DIFF_TO_MASTER" ]]; then
+## Dev site: do nothing if there's no difference to main.
+if [[ $MODE == "dev" && -z "$DIFF_TO_MAIN" ]]; then
     exit 0
 fi
 
@@ -44,7 +44,7 @@ if [[ $MODE == "prod" ]]; then
     git checkout $HIGHEST_TAG
 fi
 if [[ $MODE == "dev" ]]; then
-    git pull origin master
+    git pull origin main
 fi
 
 ## Prod and dev sites: install the application.

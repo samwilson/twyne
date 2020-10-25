526124fbfb0fc3efee0415838d00112ec9aaa0c2
Sam Wilson <sam@samwilson.id.au>
2020-10-26 07:31:01 +0800

    Management of Contacts

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -80,6 +80,16 @@ class User implements UserInterface
         return $this;
     }
 
+    public function hasRole(string $role): bool
+    {
+        return in_array($role, $this->getRoles());
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->hasRole('ROLE_ADMIN');
+    }
+
     /**
      * @see UserInterface
      */

a596959a9989a2b9b4d155d8396c63c90f30695b
Sam Wilson <sam@samwilson.id.au>
2021-09-10 17:48:47 +0800

    Add shellcheck and fix errors

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,10 +9,10 @@ fi
 
 ## Get CLI parameters.
 MODE=$1
-APP_DIR=$(cd $2; pwd)
+APP_DIR=$(cd "$2" || exit; pwd)
 
 ## Update the repo.
-cd $APP_DIR
+cd "$APP_DIR" || exit
 git fetch --quiet origin 2>&1
 
 ## Get some information about git.
@@ -22,12 +22,12 @@ CURRENT_BRANCH=$(git symbolic-ref --short -q HEAD)
 DIFF_TO_MAIN=$(git diff origin/main)
 
 ## Prod site: do nothing if we're already at the highest tag.
-if [[ $MODE == 'prod' && $CURRENT_TAG == $HIGHEST_TAG ]]; then
+if [[ $MODE == 'prod' && "$CURRENT_TAG" == "$HIGHEST_TAG" ]]; then
     exit 0
 fi
 
 ## Dev site: do nothing if not on main.
-if [[ $MODE == "dev" && $CURRENT_BRANCH != "main" ]]; then
+if [[ $MODE == "dev" && "$CURRENT_BRANCH" != "main" ]]; then
     ## Tell the maintainers, so they don't forget they're in
     ## the middle of testing something.
     echo "Dev site not on main branch. Not deploying."
@@ -41,7 +41,7 @@ fi
 
 ## Update the code.
 if [[ $MODE == "prod" ]]; then
-    git checkout $HIGHEST_TAG
+    git checkout "$HIGHEST_TAG"
 fi
 if [[ $MODE == "dev" ]]; then
     git pull origin main
